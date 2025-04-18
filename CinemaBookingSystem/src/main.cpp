335522e4b128feb7789706c34890cf6ab586f8ac
Michele-S2001 <serafini.michele01@gmail.com>
2025-04-18 11:48:29 +0200

    Update: main.cpp for more drafts

--- CinemaBookingSystem/src/main.cpp
+++ CinemaBookingSystem/src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <vector>
 #include "../include/Film.h"
 #include "../include/Auditorium.h"
+#include "../include/Projection.h"
 
 using namespace std;
 int main() {  
+  std::vector<Film> filmList;
+  std::vector<Auditorium> auditoriumList;
+
   cout << "Cinema Booking System" << endl;
   cout << "---------------------" << endl;
 
@@ -13,14 +18,38 @@ int main() {
     "ita",
     "Michele Serafini"
   );  
-  
-  film1.getFilmDetails();
 
+  Film film2 (
+    "Peace",
+    180,
+    "en",
+    "Richard Mille"
+  );  
+
+  filmList.push_back(film1);
+  filmList.push_back(film2);
+  
+  // film1.getFilmDetails();
 
   Auditorium aud1 (13, 18);
   Auditorium aud2 (15, 20);
-  aud1.getAuditoriumDetails();
-  aud2.getAuditoriumDetails();
+
+  auditoriumList.push_back(aud1);
+  auditoriumList.push_back(aud2);  
+
+  // aud1.getAuditoriumDetails();
+  // aud2.getAuditoriumDetails();
+
+  Projection projection1 (
+    filmList,
+    auditoriumList,
+    film1.getFilmID(),
+    aud1.getAuditoriumID(),
+    "24/04/2025",
+    "16:30"
+  );
+
+  projection1.getProjectionDetails();
 
   return 0;
 }
